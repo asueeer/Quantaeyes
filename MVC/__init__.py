fbebb543f7b6ad7a56fdcd132a90711480d693a8
WANGYISU <asueeer@163.com>
2019-12-10 17:55:14 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	MVC/models.py
    #	MVC/views.py
    #	app.py

--- MVC/__init__.py
+++ MVC/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy  # 一开始在pycharm的setting安装没有成功，后来在terminal上用pip install flask_sqlalchemy成功的
-from flask_login import LoginManager
+# from flask_login import LoginManager
 
 # 前端框架用bootstrap，在终端上导入pip install flask-bootstrap
 # flask上使用bootstrap指南：https://blog.csdn.net/sinat_17736151/article/details/83752483
@@ -14,8 +14,8 @@ app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.jinja_env.line_statement_prefix = '#'
 app.secret_key = 'heyurso2'
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = '/regloginpage/'
+# login_manager = LoginManager(app)
+# login_manager.login_view = '/regloginpage/'
 
 from flask_bootstrap import Bootstrap
 
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import re
+
 brief_detail = '<a href="//www.jb51.net">脚本之家</a>,Python学习！'
-dr = re.compile(r'<[^>]+>',re.S)
+dr = re.compile(r'<[^>]+>', re.S)
 dd = dr.sub('', brief_detail)
 print(dd)
